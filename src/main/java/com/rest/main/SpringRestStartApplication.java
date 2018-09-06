0c82724421a6e3d607a065db595bafab702b7c6c
Sourabh Berde <sourabh.berde44@gmail.com>
2018-09-06 22:34:29 +0530

    Start

---
+++ src/main/java/com/rest/main/SpringRestStartApplication.java
@@ -0,0 +1,12 @@
+package com.rest.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestStartApplication.class, args);
+	}
+}
