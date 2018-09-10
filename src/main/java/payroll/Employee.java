46d7ac46fadcd24f0b952c0a7a5b85d752b9c9ac
Sourabh Berde <sourabh.berde@emc.com>
2018-09-10 10:25:53 +0530

    Payroll application

--- src/main/java/payroll/Employee.java
+++ src/main/java/payroll/Employee.java
@@ -4,8 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-import lombok.Data;
-
+import lombok.*;
 @Data
 @Entity
 public class Employee {
